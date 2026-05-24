d7c31b8aa72bf3df52557f1393b1053e56741cf3
Gohar Rehman <goharrehmanfsd260@gmail.com>
2026-05-24 23:38:17 +0500

    Fix variable initialization and enhance input/output functionality

--- practice.py
+++ practice.py
@@ -1,6 +1,10 @@
 # Vairables and Data Types
-my_age = 27
 my_name = "Gohar"
+my_age = 20
 my_height = 1.75
 is_student = True
 
+# Input and Output
+my_name = input("What is Your Name: ")
+my_age = input("What is Your Age: ")
+print(f"Hello my name is {my_name}, my age is {my_age}, my height is {my_height}m and my student status is {is_student}")
