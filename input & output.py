48fb1619eeef0aeba056ce467766abedf67f55e3
Gohar Rehman <goharrehmanfsd260@gmail.com>
2026-05-25 00:11:29 +0500

    Add input/output functionality and display variable details

---
+++ input & output.py
@@ -0,0 +1,4 @@
+name = input("PLease enter your name: ")
+age = input("Please enter your age: ")
+
+print("Hello " + name + ", you are now " + age + " years old!")
--- variables.py
+++ variables.py
@@ -3,3 +3,8 @@ my_name = "Gohar Rehman"
 my_age = 20
 my_height = 5.11
 is_student  = True
+
+print("My name is " + my_name)
+print("My age is " + str(my_age))
+print("My height is " + str(my_height))
+print("I am a student: " + str(is_student))
